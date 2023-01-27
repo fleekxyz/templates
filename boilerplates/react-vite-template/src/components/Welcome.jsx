fe5653338d749fe85324190e8f3fa52e02e80046
Abril Zucchi <abrilzucchi@MacBook-Air-de-Abril.local>
2023-01-27 11:49:11 -0300

    feat: template init

---
+++ boilerplates/react-vite-template/src/components/Welcome.jsx
@@ -0,0 +1,10 @@
+import "../css/main.css"
+export default function Welcome() {
+    return (
+    <>
+    <div className="wrapper">
+    <h1>Welcome To My Boilerplate!</h1>
+    </div>
+    </>
+    );
+    }
